3a5b9db497072c70dcb700426f2a48d83a893365
Josias Fraga <josiasrs2009@gmail.com>
2023-08-08 16:42:47 -0300

    ok

--- src/Controller/PromocoesController.php
+++ src/Controller/PromocoesController.php
@@ -290,7 +290,7 @@ class PromocoesController extends AppController
             $filename = 'promocoes_' . Text::slug(strtolower($tamanhoCartaz)) . '.html';
 
             if ( $tamanhoCartaz != 'A6' ) {
-                continue;
+                //continue;
             }
 
             if ( !isset($this->poster_sizes[$tamanhoCartaz]) ) {
@@ -309,8 +309,8 @@ class PromocoesController extends AppController
             ));
             $html = $this->render('cartazes')->getBody()->__toString();
 
-            echo $html;
-            die();
+            //echo $html;
+            //die();
             //continue;
 
             $arquivos[] = [
