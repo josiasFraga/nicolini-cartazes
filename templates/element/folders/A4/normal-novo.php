dd8edf15e7509cd61d67400146ec7772478d4ccf
Josias Fraga <josiasrs2009@gmail.com>
2025-11-12 21:05:24 -0300

    em andamento

--- templates/element/folders/A4/normal-novo.php
+++ templates/element/folders/A4/normal-novo.php
@@ -1,9 +1,17 @@
+<?php
+$cabecalho_ofertas = $promocao['tipo_cartaz_slug'] . "/";
+if ($promocao->horti == "1") {
+    $cabecalho_ofertas .= "horti/";
+}
+$cabecalho_ofertas .= $dados_loja['class'];
+?>
+
 <div class="item_container <?= $dados_loja['class'] ?> <?= $promocao->horti == "1" ? "horti" : "" ?>">
 
     <div class="space_top"></div>
 
     <div class="item_header <?= $dados_loja['class'] ?>">
-        <div class="oferta_titulo">OFERTA!</div>
+        <?= $this->Html->image($cabecalho_ofertas.'.png', ['fullBase' => true]); ?>
     </div>
 
     <div class="item_name text-center font_lilita_one">
@@ -13,6 +21,7 @@
     <div class="item_price">
         <div class="item_price_to">
             <div class="item_price_to_price font_lilita_one">
+                <div class="item_price_to_price_currency">R$</div>
                 <?= number_format($promocao->VlrVenda, 2, ',', '.') ?>
                 <span class="price_to_unit_desc"><?= $promocao['un_medida'] ?></span>
             </div>
--- templates/element/folders/A4/sem-promocao.php
+++ templates/element/folders/A4/sem-promocao.php
@@ -13,6 +13,7 @@
     <div class="item_price">
         <div class="item_price_to">
             <div class="item_price_to_price font_lilita_one">
+                <div class="item_price_to_price_currency">R$</div>
                 <?= number_format($promocao->VlrVenda, 2, ',', '.') ?>
                 <span class="price_to_unit_desc"><?= $promocao['un_medida'] ?></span>
             </div>
