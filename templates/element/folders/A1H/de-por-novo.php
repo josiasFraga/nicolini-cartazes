2108ca5bd3d34ebba080f182939e0fc09ffd2a96
Josias Fraga <josias@zapshop.com.br>
2026-01-27 15:20:37 -0300

    fix-de-por-novo-a1h

--- templates/element/folders/A1H/de-por-novo.php
+++ templates/element/folders/A1H/de-por-novo.php
@@ -8,21 +8,24 @@ $list_valor = explode(",", $preco_final);
 $VlrVenda = "<div class='definitive-price-container'><div class='definitive-price'>" . $list_valor[0] . "</div><div class='cents'>," . $list_valor[1] . "</div></div>";
 ?>
 
-<div class="item_container">
+    <div class="item_container">
 
-    <div class="item_name_price">
-        <div class="item_name text-center font_lilita_one">
-            <?= $promocao->descricao_impressao ?>
-        </div>
-        
-        <div class="item_price_from">
-            <span>De</span>
-            <div class="item_price_from_price impact">
-                <?= $VlrVendaNormal ?>
-                <span class="price_from_unit_desc"><?= $promocao['un_medida'] ?></span>
+        <div class="item_name_price">
+            <div class="item_name text-center font_lilita_one">
+                <?= $promocao->descricao_impressao ?>
+            </div>
+            
+            <div class="item_price_from">
+                <span>De</span>
+                <div class="item_price_from_price impact">
+                    <?= $VlrVendaNormal ?>
+                    <span class="price_from_unit_desc"><?= $promocao['un_medida'] ?></span>
+                </div>
             </div>
         </div>
     </div>
+</div>
+<div class="row">
 
     <div class="item_price">
         <div class="item_price_to">
